f53a2df15ac6a713a31165c7db40647d4c079259
azee-rajput <azee.rajput.7@gmail.com>
2019-10-08 20:10:48 +0500

    Updates

---
+++ precache-manifest.4b4096de5842c8bbd12f92e8bb142b60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8bbb12e035afb642b1379b0d9e85f4",
+    "url": "/react-cocktail/index.html"
+  },
+  {
+    "revision": "843a20cdb5b4e70180ab",
+    "url": "/react-cocktail/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "6a7beb67d348c363943f",
+    "url": "/react-cocktail/static/css/main.f8eef940.chunk.css"
+  },
+  {
+    "revision": "843a20cdb5b4e70180ab",
+    "url": "/react-cocktail/static/js/2.b0cf2506.chunk.js"
+  },
+  {
+    "revision": "6a7beb67d348c363943f",
+    "url": "/react-cocktail/static/js/main.49de1d5d.chunk.js"
+  },
+  {
+    "revision": "ca0145fcd44672f2b6a6",
+    "url": "/react-cocktail/static/js/runtime-main.bf0d185b.js"
+  },
+  {
+    "revision": "b2e3042cfa2c3bb568f3fa2f32870797",
+    "url": "/react-cocktail/static/media/banner.b2e3042c.jpg"
+  },
+  {
+    "revision": "165558fd27008ddf8458169041fe50d0",
+    "url": "/react-cocktail/static/media/main-image.165558fd.jpg"
+  }
+]);
